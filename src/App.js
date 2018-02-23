d1a8698a783f9337e3dc0dd6d4c2525b240e50e5
Bartłomiej Sielski <b.sielski@yandex.com>
2018-02-23 20:00:04 +0100

    Info about nation's bless, temperature and growth

--- src/App.js
+++ src/App.js
@@ -8,6 +8,43 @@ import {totalBlessPoints} from './blessPoints';
 
 import './App.css';
 
+class NationInfo extends React.Component {
+  render() {
+    const fBonus = this.props.f ? " Fire +"   + this.props.f : "";
+    const aBonus = this.props.a ? " Air +"    + this.props.a : "";
+    const wBonus = this.props.w ? " Water +"  + this.props.w : "";
+    const eBonus = this.props.e ? " Earth +"  + this.props.e : "";
+    const sBonus = this.props.s ? " Astral +" + this.props.s : "";
+    const dBonus = this.props.d ? " Death +"  + this.props.d : "";
+    const nBonus = this.props.n ? " Nature +" + this.props.n : "";
+    const bBonus = this.props.b ? " Blood +"  + this.props.b : "";
+    const heat = this.props.heat ? "Farourite temperature: "  + this.props.heat : "";
+    const growth = this.props.growth ? "Default growth: "  + this.props.growth : "";
+    return (
+      <div className="nation_info">
+        <p className="nation_info__blesses">
+          {(fBonus || aBonus || wBonus || eBonus || sBonus || dBonus || nBonus || bBonus) ? "Nation bless bonuses:" : ""}
+          <span className="nation_info__blesses--fire">{fBonus}</span>
+          <span className="nation_info__blesses--air">{aBonus}</span>
+          <span className="nation_info__blesses--earth">{eBonus}</span>
+          <span className="nation_info__blesses--water">{wBonus}</span>
+          <span className="nation_info__blesses--astral">{sBonus}</span>
+          <span className="nation_info__blesses--death">{dBonus}</span>
+          <span className="nation_info__blesses--nature">{nBonus}</span>
+          <span className="nation_info__blesses--blood">{bBonus}</span>
+        </p>
+        <p className="nation_info__heat">
+          {heat}
+        </p>
+        <p className="nation_info__growth">
+          {growth}
+        </p>
+
+      </div>
+    );
+  }
+}
+
 class Debug extends React.Component {
   render() {
     return (
@@ -229,12 +266,19 @@ class App extends Component {
       <main>
         <div className="form">
           <header className="form__header">Nation</header>
-          <div className="form__body">
+          <div className="form__body form__body--nation">
             <select name="nationId" value={this.state.nationId} onChange={this.changeOption}
               className="nation-picker__select" id="nation-picker__select"
             >
               {nationOptions}
             </select>
+            <NationInfo f={this.nations[this.state.nationId].blessF}
+              a={this.nations[this.state.nationId].blessA} w={this.nations[this.state.nationId].blessW}
+              e={this.nations[this.state.nationId].blessE} s={this.nations[this.state.nationId].blessS}
+              d={this.nations[this.state.nationId].blessD} n={this.nations[this.state.nationId].blessN}
+              b={this.nations[this.state.nationId].blessB} heat={this.nations[this.state.nationId].heat}
+              growth={this.nations[this.state.nationId].growth}
+            />
           </div>
         </div>
 
